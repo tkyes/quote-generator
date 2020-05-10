f9fbd01065435395e6a87d638bfe037990c85e14
tkyes <63870721+tkyes@users.noreply.github.com>
2020-05-10 11:18:53 -0400

    Added printQuote() function.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,8 @@ Project 1 - A Random Quote Generator
 
 
 /***
- This is the quotes array, which holds the series of quotes with source, citation, and year attributes.
+ * Quotes Array:
+ * Holds the series of quotes with source, citation, and year attributes.
 ***/
 
 let quotes = [
@@ -17,7 +18,8 @@ let quotes = [
     quote: `Maybe ever’body in the whole damn world is scared of each other.` ,
     source: `John Steinbeck`,
     citation: `Of Mice and Men`,
-    year: `1937`
+    year: `1937`,
+    classic: `yes`
   },
   {
     quote: `Life is to be lived, not controlled; and humanity is won by continuing to play in face of certain defeat.`,
@@ -29,7 +31,8 @@ let quotes = [
     quote: `The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars.`,
     source: `Jack Kerouac`,
     citation: `On the Road`,
-    year: `1957`
+    year: `1957`,
+    favorite: `yes`
   },
   {
     quote: `We were the people who were not in the papers. We lived in the blank white spaces at the edges of print. It gave us more freedom. We lived in the gaps between the stories.`,
@@ -46,29 +49,35 @@ let quotes = [
 ];
 
 /***
- * Prints quotes to Console per Class Instructions.
- * console.log(quotes);
-***/
-
-/***
- * Gets a random number, uses that random number to return a random quote object.
+ * getRandomQuote function:
+ * Gets a random number
+ * Uses that random number to return a random quote object.
 ***/
 
 let randomQuote;
 
 function getRandomQuote() {
     let randomNumber = Math.floor(Math.random() * quotes.length);
-    console.log(randomNumber);
     return randomQuote = quotes[randomNumber];
 }
 
-getRandomQuote();
-
 /***
- * `printQuote` function
+ * `printQuote` function:
+ * That stores getRandomQuote in aQuote.
+ * Stores elements of the quote in relevant HTML tags.
+ * Which prints messages within the quote-box div in index.html
+
 ***/
 
+function printQuote() {
+    let aQuote = getRandomQuote();
+    let aQuoteBase = `<p class="quote">${aQuote.quote}</p>`;
+    let aQuoteSource = `<p class="source">${aQuote.source}</p>`;
+    let fullQuoteHtml = aQuoteBase + aQuoteSource;
+    return document.getElementById('quote-box').innerHTML = fullQuoteHtml;
+}
 
+printQuote();
 
 /***
  * click event listener for the print quote button
